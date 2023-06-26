a18e54e59657dd7b034a0c6a02fb06e3fa3d9426
yewon0804 <ehdnjs0804@gmail.com>
2023-06-26 18:48:49 +0900

    CrossAxisAlignment.. main axis와 cross axis를 섞어서 사용할 수 있다

--- row_and_column/lib/screen/home_screen.dart
+++ row_and_column/lib/screen/home_screen.dart
@@ -12,8 +12,9 @@ class HomeScreen extends StatelessWidget {
           bottom: false,
           child: Container( // 컨테이너, 말 그대로, 위젯들을 넣는 역할을 해요.. 형태는 child widget를 따라감
             color: Colors.black,
+            width: MediaQuery.of(context).size.width, // 이걸 이렇게 하면 size를 가져올 수 있구나~ 거기서 width나 heigth를 가져올 수 있구나~
             // child: Column(
-            child: Row(
+            child: Column(
               // MainAxisAlignment - 주축 정렬
               // start - 시작
               // end - 끝
@@ -22,6 +23,15 @@ class HomeScreen extends StatelessWidget {
               // spaceEvenly - spaceBetween과 같이 위젯을 같은 간격으로 배치하지만, 끝과 끝이, 여백으로 시작(위젯이 아니라)
               // spaceAround - spaceEvenly + 끝과 끝의 간격은 1/2
               mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+              // CrossAxisAligment - 반대축 정렬
+              // row 일 때는 세로, column 일 때는 가로~ 반대로 생각하면 편하겠죠?
+              // 주축에서는, 남는 공간을, 차지 할 수 있는 최대한을 차지한다?
+              // 근데 주축이 아닌.. 반대 축은, 위젯의 사이즈, 즉 최소한의 사이즈만 기본적으로 차지를 한다.
+
+              // baseline은 글자를 정렬할 때, 글자의 밑부분을 정렬하는건데.. (알아서 사용 하시고)
+              // crossAxisAlignment: CrossAxisAlignment.center, // 기본값 인듯
+              crossAxisAlignment: CrossAxisAlignment.stretch, // 좍좍 늘림.. 헐! 50씩만 차지하라고 했는데 최대한으로 늘어남
               children: [
                 Container(
                   color: Colors.red,
